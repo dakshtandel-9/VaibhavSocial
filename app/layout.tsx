eafc1a1f704ee0a38ffe1232740946b1d79e9fa3
Daksh Tandel <167323670+dakshtandel-9@users.noreply.github.com>
2026-04-12 21:38:02 +0530

    made website faster

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         <SmoothCursor />
         {children}
       </body>
