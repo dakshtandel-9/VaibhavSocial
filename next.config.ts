fde408152137268e6d410120ab12b0e501bc4d6e
Daksh Tandel <167323670+dakshtandel-9@users.noreply.github.com>
2026-04-29 14:44:48 +0530

    templete page updated

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   compress: true,
   images: {
     formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
   async headers() {
     return [
